4a675aca02f0d4c671f3c3737ad11baad1cf3b02
William Gillespie <wgilles1@emich.edu>
2017-02-11 01:07:36 -0500

    Simple script that shows dtw with the distance matrix and heat plot.

--- DTW/dtw_mfcc_plot.py
+++ DTW/dtw_mfcc_plot.py
@@ -12,9 +12,6 @@ y_2, sampling_rate_2 = librosa.load(file_path_2)
 mfcc_1 = librosa.feature.mfcc(y_1, sampling_rate_1, n_mfcc=13).T
 mfcc_2 = librosa.feature.mfcc(y_2, sampling_rate_2, n_mfcc=13).T
 
-# x = np.array([0, 0, 1, 1, 2, 4, 2, 1, 2, 0]).reshape(-1, 1)
-# y = np.array([1, 1, 1, 2, 2, 2, 2, 3, 2, 0]).reshape(-1, 1)
-
 dist, cost, acc, path = dtw(mfcc_1, mfcc_2, dist=lambda a, b: np.linalg.norm(a - b, ord=1))
 print("dist: ", dist)
 
