d217a2e7013e98f4e9ae62125ad484a9b2ec98c9
William Gillespie <wgilles1@emich.edu>
2017-02-13 00:40:02 -0500

    Added some features to the plot (title, x y label, legend).

--- DTW/dtw_test_bridge_plot.py
+++ DTW/dtw_test_bridge_plot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import dtw as dtw
 
 """
@@ -11,7 +12,7 @@ I assume it is called a bridge plot because it looks like a bridge (kind of).
 samples = 200
 x = np.linspace(0, 2*np.pi, samples + 1)
 
-sin_offset = 2
+sin_offset = 0
 sin_x = np.sin(x) + sin_offset
 sin_x = sin_x.reshape(-1, 1)
 cos_x = np.cos(x).reshape(-1, 1)
@@ -27,11 +28,23 @@ cos_0_2pi_scale = np.array([q / (samples / (2 * np.pi)) for q in path[1]])
 
 sin_x_mapping = np.sin(sin_0_2pi_scale) + sin_offset
 cos_x_mapping = np.cos(cos_0_2pi_scale)
-plt.plot(x, sin_x)
-plt.plot(x, cos_x)
+plt.plot(x, sin_x, linewidth=3, c='red')
+plt.plot(x, cos_x, linewidth=3, c='blue')
+
+sin_patch = mpatches.Patch(color='red', label='Sin(x)')
+cos_patch = mpatches.Patch(color='blue', label='Cos(x)')
+patches = [sin_patch, cos_patch]
+plt.legend(handles=patches, loc='best', fontsize='x-large')
 
 # this for loop plots all of the lines associated with the warping.
 for i in range(0, len(sin_0_2pi_scale)):
     plt.plot([sin_0_2pi_scale[i], cos_0_2pi_scale[i]], [sin_x_mapping[i], cos_x_mapping[i]], 'k-')
+
+plt.xlim((min(x), max(x)))
+
+plt.title("Sin(x) vs. Cos(x) bridge plot", fontsize=20)
+plt.xlabel("x", fontsize=14)
+plt.ylabel("y", fontsize=14)
+
 
 plt.show()
